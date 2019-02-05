217dbc4fa09c4ba9ae448f5d6cb46a1d02e86ede
kne-cr <yes6sss1@gmail.com>
2019-02-05 19:21:01 +0900

    立方体を表示

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,22 @@
 import * as THREE from 'three';
 
-const width = 500;
-const height = 500;
-
-// scene
 const scene = new THREE.Scene();
 
-// mesh
-const geometry = new THREE.SphereGeometry(5, 60, 40);
-geometry.scale(-1, 1, 1);
+const box = new THREE.Mesh(new THREE.BoxGeometry(40, 40, 40), new THREE.MeshNormalMaterial( { color: 0xff0000 } ));
+box.rotation.x = 60;
+box.rotation.y = 45;
+box.rotation.z = 30;
 
-const material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+scene.add(box);
 
- const sphere = new THREE.Mesh(geometry, material);
- scene.add(sphere);
+const camera = new THREE.PerspectiveCamera(75, ,window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(0, 0, 100);
+camera.lookAt(box.position);
 
- //camera
- const camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000);
- camera.position.set(0, 0, 0.1);
- camera.lookAt(sphere.position);
+// render
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+document.getElementById("myCanvas").appendChild(renderer.domElement);
 
- // render
- const renderer = new THREE.WebGLRenderer();
- renderer.setSize(width, height);
- renderer.setClearColor(0x000000);
- document.getElementById("myCanvas").appendChild(renderer.domElement);
-
- renderer.render(scene, camera);
+renderer.render(scene, camera);
